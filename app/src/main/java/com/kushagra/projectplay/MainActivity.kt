a58c239a80aa518be640c3a3113a595dbc9e4904
Kushagra <kushagra.acharya@outlook.com>
2021-01-31 12:48:50 +0545

    initial commit

---
+++ app/src/main/java/com/kushagra/projectplay/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kushagra.projectplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kushagra/projectplay/jetpack/datastore/DataStoreActivity.kt
@@ -0,0 +1,12 @@
+package com.kushagra.projectplay.jetpack.datastore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kushagra.projectplay.R
+
+class DataStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_store)
+    }
+}
