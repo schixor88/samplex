956def69a8f1a9100cb9c62e5f78e06bfa3d80c4
schixor88 <kushagra.acharya@outlook.com>
2021-02-05 12:27:09 +0545

    test branching

--- .github/scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-echo Hi
-pip install requests
-git clone https://github.com/mega-arbuz/apk-release-deploy.git
-python apk-release-deploy/deploy.py
--- ci_deploy.py
+++ ci_deploy.py
@@ -12,7 +12,7 @@ from dropbox.exceptions import ApiError, AuthError
 TOKEN = 'KCKpFCbHXK0AAAAAAAAAAWIyE3FdKpYF8ozB9NhOUC3p5p1D0M6R6yIqShI0uEJb'
 
 LOCALFILE = 'app/build/outputs/apk/debug/app-debug.apk'
-BACKUPPATH = '/samplex' # Keep the forward slash before destination filename
+BACKUPPATH = '/sample.apk' # Keep the forward slash before destination filename
 
 
 # Uploads contents of LOCALFILE to Dropbox
